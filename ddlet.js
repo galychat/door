947e1d2f383b03a7d9dc206c89db122d845854ca
galychat <42301824+galychat@users.noreply.github.com>
2018-08-19 23:38:43 +0300

    Create ddlet.js

---
+++ ddlet.js
@@ -0,0 +1,48 @@
+setTimeout (function (){   $("<div class='fr borderg' style='padding:2px;background-color:white;margin-right:4px;'>تصميم وبرمجه: <a href='https://jawalhost.com/'>جوال هوست</a></div>").insertAfter($(".loginstat"));  },1000);
+
+if(location.protocol!='https:'){location.protocol='https:';}
+$('.break.light').css({"background-image":"url(http://alashaaaq.com/sico/z1ch6nnbcn10.PNG)"})
+$(`<img src="https://galychat.com/sico/z1cl9rree410.gif" class="fr" style="width:100%;margin-top: 1px;">
+<div style="text-align:center;background: #e6ccd3;">
+<a href="http://alashaacdhost.com"><img src=https://galychat.com/sico/z1cl9t4mg010.gif?v=2018" height="25" width="120" alt="شات"></a>
+<a target="_blank" href="sub.html">
+<img src=https://galychat.com/sico/z1cl9t4qdp10.gif?v=2018" height="25" width="120"></a>
+<a href="https://play.google.com/store/ds/details?id=com.w_7578132"><img src=https://galychat.com/sico/z1cl9t4bgr10.gif?v=2018" height="25" width="120"></a>
+<a href="javascript:void(0);" Onclick="ShowHide5();"><img src=https://www.alashaaaq.com/sico/z1ch6nlocl10.PNG?v=2018" height="25" width="200"></a>
+</div><div style="text-align:center;color: #fff; padding: 2px; background: #7B1B11; max-width: 290px; border-radius: 12px; text-align: center; margin-right: auto; margin-left: auto; font-size:25px;  display: none;" name="help" ID="help5">
+يمكنك التواصل مع ادارة الشات عن طريق خدمات التواصل<br><a href="zain">zain </a>
+<br>او<br>BBM:  ..<br>
+<a title="...." href="/">...</a>,<a title="ع" href="/">....</a>,<a title="...href="/"...</a></div>
+<center>
+<img src=https://galychat.com/sico/z1cl9rree410.gif  WIDTH=250 HEIGHT=50><br>(&#127801;<font size="3">غــــ&#127801;ــــالي</font>)&#127801;
+<marquee direction="Up" width="100%" height="100" scrollAmount="1" align="center" style="border:#7B1B11 solid 2px;"><font size="2" color="#750000"><center>
+<br>نجــــوم شات غالي  للجــــــوال
+<br><br><img src="https://galychat.com/sico/z1cl5g72ao10.gif?v=2018" alt="نجم الموقع" height="30" width="100">
+<br><br>(&#127801;<img src="https://www.alashaaaq.com/sico/z1ck33sblf10.gif?v=2018" alt="صورة نجم الموقع" height="30" width="100">&#127801;)
+<br><br><img src="https://www.alashaaaq.com/sico/z1chlsuof110.gif?v=2018" alt="ملكة الحائط" height="30" width="100">
+<br><br>(&#127801;<img src="https://galychat.com/sico/z1cl5g72ao10.gif?v=2018" alt="صورة ملك الحائط" height="30" width="100">&#127801;)
+<br><br><img src="https://www.alashaaaq.com/sico/z1cjgr7acj10.gif?v=2018" alt="افضل تواجد" height="30" width="100">
+<br><br>(&#127801;<img src="https://galychat.com/sico/z1cl5g72ao10.gif?v=2018" alt="صورة افضل تواجد" height="30" width="100">&#127801;)
+<br><br><img src="https://www.alashaaaq.com/sico/z1cjgr6t6t10.gif?v=2018" alt="افضل سوبر" height="30" width="100">
+<br><br>(&#127801;<img src="https://galychat.com/sico/z1cl5g72ao10.gif?v=2018" alt="صورة افضل سوبر" height="30" width="100">&#127801;)
+<br>لكـــم تحيات ادارة غالي وغالي للجوال</font></marquee>
+`).insertBefore('.nav-tabs');
+$(".label-primary" ).css( "border-radius", "15px 15px 15px 15px");
+$('.nav-tabs>li>a').css('color','#7B1B11')
+$( "a.label.label-primary.fl > img.fl" ).replaceWith('<img src="https://www.alashaaaq.com/sico/z1ci5otiie10.png" class="fl" style="margin-Right: 2px; width: 30px;">');
+$("[data-target=#wall]").attr('title','الحائط يـمـنـع السوالف فيه لـو تكرمتم')
+$('<img src="https://www.alashaaaq.com/sico/z1ci75d13d10.png" style="width:90px;margin-top:-90px; " class="fr">').insertBefore('.loginstat');
+$("<br>").insertBefore('.checkbox');
+$(`<link href="https://fonts.googleapis.com/css?family=Changa" rel="stylesheet">
+<marquee direction="right" width="65%" direction="right" style="margin-bottom: -7px;font-family: 'Changa', sans-serif;    height: 20px;color: #ffffff;"> حيـاكـم الله في شات غالي وغالي.. نتشرف بتواجدكم .. نقدم لكم ( مسابقات ) - ( العاب ) - ( ترفيه ) ونتمنى لكم قضاء اجمل الاوقات معنا </marquee></font></b></td>
+`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success');
+$(document).ready($("div.fr.borderg").css("display","none"));
+$('body').css({"background-image":"url()"});
+(document.getElementById("tbox").placeholder = "اكتب هنا");
+$(`<center><div width="100%" style="background-color: #750000;color: #ffffff;border-radius: 30px 30px 0px 0px;">تحميل تطبيـق الشات <a title="تحميل تطبيق" href="https://play.googleom/store/apps/details?id=com.w_7578132">اضغط هنا</a>
+</div><center>`).insertBefore('#d2');function ShowHide5() { var curtable = document.getElementById("help5"); if (curtable.style.display == "none") { curtable.style.display = "block"; } else { curtable.style.display = "none"; } }
+$(`<style> .fitimg.fl.u-pic {border-radius: 50px;} 
+</style>`).insertBefore('body');
+</script>
+</body>
+</html>
